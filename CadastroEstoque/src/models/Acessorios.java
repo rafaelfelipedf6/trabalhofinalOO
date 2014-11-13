475a42d4f55acec117de1cf32054c35f6f1b6420
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-11-13 09:35:13 -0200

    Adicionando getters e setters da classe Acessorios assim tambem como seus atributos

--- CadastroEstoque/src/models/Acessorios.java
+++ CadastroEstoque/src/models/Acessorios.java
@@ -1,5 +1,18 @@
 package models;
 
 public class Acessorios extends Produto {
-
+	String marca;
+	String utilidade;
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getUtilidade() {
+		return utilidade;
+	}
+	public void setUtilidade(String utilidade) {
+		this.utilidade = utilidade;
+	}
 }
