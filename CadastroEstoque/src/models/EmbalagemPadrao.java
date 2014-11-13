75476ddc216582f07f5412b00ab5870540615d11
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-11-13 09:32:30 -0200

    Adicionando atributos a classe EmbalagemPadrao

--- CadastroEstoque/src/models/EmbalagemPadrao.java
+++ CadastroEstoque/src/models/EmbalagemPadrao.java
@@ -1,5 +1,39 @@
 package models;
 
 public class EmbalagemPadrao {
-
+	float largura;
+	float altura;
+	float profundidade;
+	float custo;
+	float fornecedor;
+	public float getLargura() {
+		return largura;
+	}
+	public void setLargura(float largura) {
+		this.largura = largura;
+	}
+	public float getAltura() {
+		return altura;
+	}
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	public float getProfundidade() {
+		return profundidade;
+	}
+	public void setProfundidade(float profundidade) {
+		this.profundidade = profundidade;
+	}
+	public float getCusto() {
+		return custo;
+	}
+	public void setCusto(float custo) {
+		this.custo = custo;
+	}
+	public float getFornecedor() {
+		return fornecedor;
+	}
+	public void setFornecedor(float fornecedor) {
+		this.fornecedor = fornecedor;
+	}
 }
