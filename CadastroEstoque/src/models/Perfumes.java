edbc59ebf4b760cc2372e54ab8742fa8ca21b835
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-11-13 09:29:43 -0200

    Adicionando Atributos, getters e setters na classe Perfumes

--- CadastroEstoque/src/models/Perfumes.java
+++ CadastroEstoque/src/models/Perfumes.java
@@ -1,5 +1,34 @@
 package models;
 
 public class Perfumes extends Produto {
-
+	String marca;
+	String volume;
+	String validade;
+	String impostoMl;
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getVolume() {
+		return volume;
+	}
+	public void setVolume(String volume) {
+		this.volume = volume;
+	}
+	public String getValidade() {
+		return validade;
+	}
+	public void setValidade(String validade) {
+		this.validade = validade;
+	}
+	public String getImpostoMl() {
+		return impostoMl;
+	}
+	public void setImpostoMl(String impostoMl) {
+		this.impostoMl = impostoMl;
+	}
+	
 }
