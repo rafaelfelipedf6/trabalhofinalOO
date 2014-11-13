11354ea0c25bcda217aef93d09c72eb1db1cd79a
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-11-13 09:18:58 -0200

    Adicionando Getters e Setters da clase Usuario assim também como seus atributos

--- CadastroEstoque/src/models/Usuario.java
+++ CadastroEstoque/src/models/Usuario.java
@@ -1,5 +1,40 @@
 package models;
 
 public class Usuario {
-
+	String nome;
+	String Cpf;
+	String Rg;
+	String Cargo;
+	String tipoAcesso;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return Cpf;
+	}
+	public void setCpf(String cpf) {
+		Cpf = cpf;
+	}
+	public String getRg() {
+		return Rg;
+	}
+	public void setRg(String rg) {
+		Rg = rg;
+	}
+	public String getCargo() {
+		return Cargo;
+	}
+	public void setCargo(String cargo) {
+		Cargo = cargo;
+	}
+	public String getTipoAcesso() {
+		return tipoAcesso;
+	}
+	public void setTipoAcesso(String tipoAcesso) {
+		this.tipoAcesso = tipoAcesso;
+	}
 }
